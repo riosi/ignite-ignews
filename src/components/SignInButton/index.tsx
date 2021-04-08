ddd8d6b4cb8175ff540ec69940fba830e62198c9
Ingryd Rios <riosingryd@gmail.com>
2021-04-08 17:17:52 -0300

    :passport_control: finished next authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,24 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut ,useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    console.log(session)
+
+    return session ? (
 
         <button 
         type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signOut()}
+        >
             <FaGithub color="#04d361" />
-            Ingryd Rios
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
 
@@ -21,7 +26,8 @@ export function SignInButton() {
 
         <button 
         type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
